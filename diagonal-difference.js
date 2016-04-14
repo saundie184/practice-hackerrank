259687a656ec76d96f5bbe42def9a8de9cadb384
Saundie Weiss <saundieweiss@gmail.com>
2016-04-14 11:31:28 -0600

    Initial commit

---
+++ diagonal-difference.js
@@ -0,0 +1,12 @@
+// Given a square matrix of size N×NN×N, calculate the absolute difference between the sums of its diagonals.
+//
+// Input Format
+//
+// The first line contains a single integer, NN.
+// The next NN lines denote the matrix's rows, with each line containing NN space-separated integers describing the columns.
+  //
+// Output Format
+//
+// Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
+
+'use strict';
