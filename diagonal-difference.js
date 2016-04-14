61a1b4ec0dd1a0ba7631164116cab44fb1b7242f
Saundie Weiss <saundieweiss@gmail.com>
2016-04-14 11:43:02 -0600

    Just started

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -9,4 +9,20 @@
 //
 // Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
 
+// Sample Input
+// 3
+// 11 2 4
+// 4 5 6
+// 10 8 -12
+
+// Sample Output
+// 15
+
 'use strict';
+
+function diagonals(num, string){
+//split the string by every num character
+
+}
+
+console.log(diagonals(3,'11 2 4 4 5 6 10 8 -12'));
