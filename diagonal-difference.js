1e2f5e878f1d4b00e3e1e74141c2a1133d73cd2c
Saundie Weiss <saundieweiss@gmail.com>
2016-04-16 17:24:34 -0600

    Working on diagonals

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -23,6 +23,8 @@
 function diagonals(num, string){
 //split the string by every num character
 //split that string by ' '
+
+//create a sum of the diagonals
 }
 
 console.log(diagonals(3,'11 2 4 4 5 6 10 8 -12'));
