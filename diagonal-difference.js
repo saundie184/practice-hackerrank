a396b711d2d4db0aba45b4f3e8f9fb20e889a83a
Saundie Weiss <saundieweiss@gmail.com>
2016-04-18 15:21:10 -0600

    Created for loop

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -4,7 +4,7 @@
 //
 // The first line contains a single integer, NN.
 // The next NN lines denote the matrix's rows, with each line containing NN space-separated integers describing the columns.
-  //
+//
 // Output Format
 //
 // Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
@@ -20,13 +20,20 @@
 
 'use strict';
 
-function diagonals(num, string){
-//split the string by every num character
-//split that string by ' '
+function diagonals(num, string) {
+    //split the string by every number that is after a space
+    for(var i = 0; i < string.length; i++){
+      // console.log(string[i]);
+      if(string[i] === ' '){
+        console.log(string[i+1]);
+      }
+    }
+    // return string;
+    //split that string by ' '
 
-//create a sum of the diagonals
+    //create a sum of the diagonals
 
-//return the largest sum
+    //return the largest sum
 }
 
-console.log(diagonals(3,'11 2 4 4 5 6 10 8 -12'));
+console.log(diagonals(3, '11 2 4 4 5 6 10 8 -12'));
